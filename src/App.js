694aeaa6f275bdd3966453a2d622bd7a0c33b3c9
Nikul54 <npsbhs@gmail.com>
2025-03-17 23:28:51 -0400

    Updating login route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import SearchDog from './Components/SearchDog/SearchDog';
 
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/search" element={<SearchDog />} />
       </Routes>
     </Router>
